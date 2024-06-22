6453235da059c92981fd07e6a732e10c569636c3
LukeZGD <26163116+LukeZGD@users.noreply.github.com>
2024-06-22 08:59:30 +0800

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./autogen.sh libplist_CFLAGS="-I/opt/local/include" libplist_LIBS="-L/opt/local/lib -lplist-2.0" libgeneral_CFLAGS="-I/usr/local/include" libgeneral_LIBS="-L/usr/local/lib -lgeneral"
+
